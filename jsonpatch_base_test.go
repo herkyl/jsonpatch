050e9965acac0a73b1ec0bed3ba7daeffc2a8423
herkyl <s.herkyl@gmail.com>
2019-06-02 20:43:37 +0300

    WIP

--- jsonpatch_base_test.go
+++ jsonpatch_base_test.go
@@ -1,67 +1,33 @@
 package jsonpatch
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"testing"
 
-	evanphx "github.com/evanphx/json-patch"
 	"github.com/stretchr/testify/assert"
 )
 
-type JSONTest struct {
-	Comment  string      `json:"comment"`
-	Original interface{} `json:"doc"`
-	Target   interface{} `json:"expected"`
-	Disabled bool        `json:"disabled"`
-	Error    *string     `json:"error"`
+func TestNoChange(t *testing.T) {
+	patch, e := CreatePatch([]byte(`{"a":100, "b":200, "c":"hello"}`), []byte(`{"a":100, "b":200, "c":"hello"}`))
+	assert.NoError(t, e)
+	assert.Equal(t, 0, len(patch))
 }
 
-// evanphx/json-patch fails when replacing elements on root level, let's wrap everything into an object
-type RootWrap struct {
-	Root interface{} `json:"root"`
+func TestAddingToArray(t *testing.T) {
+	patch, e := CreatePatch([]byte(`{"a":[1, 2, 3]}`), []byte(`{"a":[1, 2, 3, 4]}`))
+	assert.NoError(t, e)
+	assert.Equal(t, 1, len(patch))
+	p := patch[0]
+	assert.Equal(t, "add", p.Operation)
+	assert.Equal(t, "/a/3", p.Path)
+	assert.Equal(t, float64(4), p.Value)
 }
 
-func TestBase(t *testing.T) {
-	file, err := ioutil.ReadFile("tests.json")
-	assert.NoError(t, err)
-
-	var jsonTests []JSONTest
-	err = json.Unmarshal([]byte(file), &jsonTests)
-	assert.NoError(t, err)
-
-	for i, tc := range jsonTests {
-		if tc.Disabled || tc.Error != nil || tc.Original == nil || tc.Target == nil {
-			continue
-		}
-		testName := fmt.Sprintf(`Test #%d %s`, i, tc.Comment)
-		tc.Original = RootWrap{tc.Original}
-		tc.Target = RootWrap{tc.Target}
-		t.Run(testName, func(t *testing.T) {
-			b1, err := json.Marshal(tc.Original)
-			assert.NoError(t, err)
-			b2, err := json.Marshal(tc.Target)
-			assert.NoError(t, err)
-
-			patch, err := CreatePatch(b1, b2)
-			assert.NoError(t, err)
-			pb, err := json.Marshal(patch)
-			assert.NoError(t, err)
-			t.Log("original", string(b1))
-			t.Log("target", string(b2))
-			for i, p := range patch {
-				b, _ := json.Marshal(p)
-				t.Log("diff", i, string(b))
-			}
-
-			ep, err := evanphx.DecodePatch(pb)
-			assert.NoError(t, err)
-
-			modified, err := ep.Apply(b1)
-			assert.NoError(t, err)
-
-			assert.Equal(t, b2, modified)
-		})
-	}
+func TestReplaceScalars(t *testing.T) {
+	patch, e := CreatePatch([]byte(`1`), []byte(`"s"`))
+	assert.NoError(t, e)
+	assert.Equal(t, 1, len(patch))
+	p := patch[0]
+	assert.Equal(t, "replace", p.Operation)
+	assert.Equal(t, "", p.Path)
+	assert.Equal(t, "s", p.Value)
 }
---
+++ jsonpatch_optimise_test.go
@@ -0,0 +1,42 @@
+package jsonpatch
+
+import (
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+// Test should replace the entire array instead of doing separate array operations
+func TestReplaceInsteadOfArrayOps(t *testing.T) {
+	patch, e := CreatePatch([]byte(`{"a":[1, 2, 3]}`), []byte(`{"a":[1, 0, 0]}`))
+	assert.NoError(t, e)
+	log.Println(patch)
+	assert.Equal(t, 1, len(patch))
+	p := patch[0]
+	assert.Equal(t, "replace", p.Operation)
+	assert.Equal(t, "/a", p.Path)
+	assert.Equal(t, []interface{}{float64(1), float64(0), float64(0)}, p.Value)
+}
+
+// Test should do individual array operations because one of the constant values is too big for an efficient replace
+func TestReplaceObjectInArray(t *testing.T) {
+	a := fmt.Sprintf(`[1, 2, {"a": "%s", "b": "2"}]`, lorem)
+	b := fmt.Sprintf(`[1, 2, {"a": "%s", "b": "1", "c": "3"}]`, lorem)
+	patch, e := CreatePatch([]byte(a), []byte(b))
+	assert.NoError(t, e)
+	log.Println(patch)
+	assert.Equal(t, 2, len(patch))
+	p1 := patch[0]
+	assert.Equal(t, "replace", p1.Operation)
+	assert.Equal(t, "/2/b", p1.Path)
+	assert.Equal(t, "1", p1.Value)
+
+	p2 := patch[1]
+	assert.Equal(t, "add", p2.Operation)
+	assert.Equal(t, "/2/c", p2.Path)
+	assert.Equal(t, "3", p2.Value)
+}
