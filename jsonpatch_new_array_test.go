313b0132cc4a1a7bc63765c41ad7e5815880396d
herkyl <s.herkyl@gmail.com>
2019-06-06 17:11:02 +0300

    More tests

--- jsonpatch_new_array_test.go
+++ jsonpatch_new_array_test.go
@@ -66,3 +66,71 @@ func TestAddingCenterElement(t *testing.T) {
 	assert.Equal(t, "/1", change.Path)
 	assert.Equal(t, "b", change.Value)
 }
+
+func TestAddingElementToEnd(t *testing.T) {
+	patch, e := diffArrays(
+		[]interface{}{"a", "b"},
+		[]interface{}{"a", "b", "c"},
+		"",
+		true,
+	)
+	assert.NoError(t, e)
+	t.Log("Patch:", patch)
+	assert.Equal(t, 1, len(patch))
+
+	change := patch[0]
+	assert.Equal(t, "add", change.Operation)
+	assert.Equal(t, "/2", change.Path)
+	assert.Equal(t, "c", change.Value)
+}
+
+func TestAddingElementToStart(t *testing.T) {
+	patch, e := diffArrays(
+		[]interface{}{"a", "b"},
+		[]interface{}{"0", "a", "b"},
+		"",
+		true,
+	)
+	assert.NoError(t, e)
+	t.Log("Patch:", patch)
+	assert.Equal(t, 1, len(patch))
+
+	change := patch[0]
+	assert.Equal(t, "add", change.Operation)
+	assert.Equal(t, "/0", change.Path)
+	assert.Equal(t, "0", change.Value)
+}
+
+func TestRemovingLastElement(t *testing.T) {
+	patch, e := diffArrays(
+		[]interface{}{"a", "b", "c"},
+		[]interface{}{"a", "b"},
+		"",
+		true,
+	)
+	assert.NoError(t, e)
+	t.Log("Patch:", patch)
+	assert.Equal(t, 1, len(patch))
+
+	change := patch[0]
+	assert.Equal(t, "remove", change.Operation)
+	assert.Equal(t, "/2", change.Path)
+	assert.Equal(t, nil, change.Value)
+}
+
+func TestRemovingFirstElement(t *testing.T) {
+	patch, e := diffArrays(
+		[]interface{}{"a", "b", "c"},
+		[]interface{}{"b", "c"},
+		"",
+		true,
+	)
+	assert.NoError(t, e)
+	t.Log("Patch:", patch)
+	assert.Equal(t, 1, len(patch))
+
+	change := patch[0]
+	assert.Equal(t, "remove", change.Operation)
+	assert.Equal(t, "/0", change.Path)
+	assert.Equal(t, nil, change.Value)
+}
